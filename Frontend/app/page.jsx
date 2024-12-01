ac3093aa5aa0b6232dc9eac50e5d08849c324281
amr <amrashraf1592@gmail.com>
2024-12-01 22:58:44 +0200

    amr ashraf

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -1,14 +1,28 @@
 import NavBar from "@components/NavBar";
 
+const Header=()=>{
+    return(
+        <div>
+
+        </div>
+    );
+
+};
 const Home = () => {
     return (
         <div>
             <div className='bg-[#F5F5F5] w-full h-screen'>
                 <div className='w-[92%] mx-auto pt-4'>
                     <NavBar />
+                    <Header/>
+
+                    
+                    <div></div>
+                    
                 </div>
             </div>
         </div>
+       
     );
 };
 
