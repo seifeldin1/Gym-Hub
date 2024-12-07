d6649c9cf64f80405ec7669a43a0b4a7a2ada989
seifeldin1 <seif1442004@gmail.com>
2024-12-07 17:37:50 +0200

    enhanced login page

--- Frontend/app/login/page.jsx
+++ Frontend/app/login/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from "react";
-import { Button, IconButton, TextField } from "@node_modules/@mui/material";
+import { Button, IconButton, TextField , Tooltip } from "@node_modules/@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import LightModeIcon from '@mui/icons-material/LightMode';
@@ -53,64 +53,77 @@ const LoginPage = () => {
         <div className={`min-h-screen flex justify-center p-8 items-center bg-cover ${style.background} transition-colors`} >
             {/*form of login*/}
 
-            <div className={`relative p-4 items-center rounded-lg shadow-lg w-80 max-w-lg transition-colors ${darkMode ? 'bg-[#272A37] text-white' : 'bg-white text-black'}`}>
+            <div className={`relative p-4 items-center rounded-lg shadow-lg w-80 max-w-lg transition-colors ${darkMode ? 'rgba(39, 42, 55, 0)' : 'rgba(255, 255, 255, 0.8)'}`} 
+            style={{ transform: 'translate(55%, 30%)'}}>
                 <IconButton
                     onClick={ChangeDarkMode}
-                    color="inherit"
-                    className={`absolute top-8 right-1 ${darkMode ? `bg-gray-800` : `bg-white`}`}
+                    style={{
+                        maxWidth: '32px', 
+                        maxHeight: '32px', 
+                        minWidth: '20px',
+                        minHeight: '20px',
+                        color: darkMode ? 'white' : '#1E1E2A', // Yellow in dark mode
+                        backgroundColor: darkMode ? '#1E1E2A ' : '#FCEE02', // Adjust button background
+                    }}
+                    className={`absolute top-9 left-10 bg-white transition-colors`}
                 >
                     {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
                 </IconButton>
                 <div className={`w-[100%] mb-10`} >
-                    <h2 className={`text-xl sm:text-2xl font-bold mb-5 text-center`}>
+                    <h2 className={`text-xl sm:text-2xl color:#110F10 font-bold mb-5 text-center`}>
                         Log in
                     </h2>
                     <form onSubmit={LoginHandler} className="m-4 mb-8">
                         <div className="mb-4">
+                        <Tooltip title="Enter your username" arrow>
                             <TextField
                                 label="Username"
                                 variant="outlined"
+                                placeholder="Enter Username"
                                 value={username}
                                 fullWidth
                                 onChange={(e) => setUsername(e.target.value)}
                                 InputLabelProps={{
                                     sx: {
-                                        color: darkMode ? 'grey' : 'black', // Default label color
+                                        color: darkMode ? 'white' : '#FDE047', // Default label color
                                         '&.Mui-focused': {
-                                            color: darkMode ? '#1976D2' : 'black', // Color when focused
+                                            color: darkMode ? 'white' : '#FDE047', // Color when focused
                                         },
                                     },
                                 }}
                                 InputProps={{
-                                    style: { color: darkMode ? 'white' : 'black', backgroundColor: darkMode ? '#374151' : '#ffffff', height: '100%' },
+                                    style: { color: darkMode ? 'white' : 'yellow', backgroundColor: darkMode ? 'rgba(39, 42, 55, 0.7)' : 'rgba(29, 27, 28, 0.7)', height: '100%' , borderRadius:'8px' },
                                 }}
                                 className="rounded">
                             </TextField>
+                            </Tooltip>
                             {UsernameError && !username && <p className="text-red-700">{UsernameError}</p>}
                         </div>
                         <div className={`flex ${darkMode ? `text-white` : `text-black`}`}>
+                        <Tooltip title="Enter your password" arrow>
                             <TextField
                                 label="Password"
                                 variant="outlined"
+                                placeholder="Enter Password"
                                 type={showPassword ? 'text' : 'password'}
                                 value={password}
                                 fullWidth
                                 onChange={(e) => setPassword(e.target.value)}
                                 InputLabelProps={{
                                     sx: {
-                                        color: darkMode ? 'grey' : 'black', // Default label color
+                                        color: darkMode ? 'white' : '#FDE047', // Default label color
                                         '&.Mui-focused': {
-                                            color: darkMode ? '#1976D2' : 'black', // Color when focused
+                                            color: darkMode ? 'white' : '#FDE047', // Color when focused
                                         },
                                     },
                                 }}
                                 InputProps={{
-                                    style: { color: darkMode ? 'white' : 'black', backgroundColor: darkMode ? '#374151' : '#ffffff', height: '100%' },
+                                    style: { color: darkMode ? 'white' : 'yellow', backgroundColor: darkMode ? 'rgba(39, 42, 55, 0.7)' : 'rgba(29, 27, 28, 0.7)', height: '100%' , borderRadius:'8px' },
                                     endAdornment: (
                                         <IconButton
                                             onClick={ChangePasswordVisibility}
                                             edge="end"
-                                            style={{ color: darkMode ? 'white' : 'black' }}
+                                            style={{ color: darkMode ? 'white' : 'yellow' }}
                                         >
                                             {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
                                         </IconButton>
@@ -119,6 +132,7 @@ const LoginPage = () => {
                                 className="rounded">
 
                             </TextField>
+                            </Tooltip>
 
 
 
@@ -145,11 +159,13 @@ const LoginPage = () => {
                             disabled={loading}
                             onClick={LoginHandler}
                             fullWidth
-                            className="w-60"
+                            className="w-60 text-black"
                             sx={{
-                                backgroundColor: "#1D90F5", // Custom background color
+                                backgroundColor: "yellow", // Custom background color
+                                color:"black",
                                 '&:hover': {
-                                    backgroundColor: "#1565C0", // Custom hover color
+                                    backgroundColor: "black", // Custom hover color
+                                    color:"yellow"
                                 },
                             }}
                             >
@@ -158,7 +174,7 @@ const LoginPage = () => {
                         </Button>
                     </div>
 
-                    {success && <p className="text-green-500 mt-2">{success}</p>}
+                    {success && <p className="text-yellow-200 mt-2 p-4">{success}</p>}
 
 
                 </div>
