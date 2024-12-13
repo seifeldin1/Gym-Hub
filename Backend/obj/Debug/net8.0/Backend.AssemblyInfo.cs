0f4f963d666f03128d9253a219811e02f784be73
amr <amrashraf1592@gmail.com>
2024-12-13 18:55:05 +0200

    renamed all files of services and adding supplementsController

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6995c4cb3a1039e958218f66117be48033125f03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0672c60ff33f9c6a89ff7f65ea54f7d37e16e8e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
