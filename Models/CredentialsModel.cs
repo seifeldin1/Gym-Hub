244988c7a3194261b7d4635e32b61495230d394b
Dakroury <77912843+seifeldin1@users.noreply.github.com>
2024-12-19 17:12:33 +0200

    Delete Models directory

--- Models/CredentialsModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Backend.Models{
-    public class Credentials{
-        public string Username { get; set; }
-        public string Password { get; set; }
-
-        public string? Type {get; set;}
-    }
-}
--- Models/SkillsModel.cs
+++
@@ -1,6 +0,0 @@
-namespace Backend.Models{
-    public class Skills{
-        public string skillName {get; set;}
-        public string coachName {get; set;}
-    }
-}
