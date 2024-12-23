808adeae82254bc6983c870746ab8f4e8a487a4a
seifeldin1 <seif1442004@gmail.com>
2024-12-23 18:21:22 +0200

    Sign up page 90% done

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ac26d8c2fc34e35676142096b1768705915e95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53452969e38784dc9b2d847f1fe5550496e9da3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
