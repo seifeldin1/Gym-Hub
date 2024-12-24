6abad9eddfc78f40adcab8ca9825de3edde1e98e
seifeldin1 <seif1442004@gmail.com>
2024-12-24 17:56:24 +0200

    sign up page , page 1 done

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53452969e38784dc9b2d847f1fe5550496e9da3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a508818a5ef7b8682110aff236ff0b684551c44d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
