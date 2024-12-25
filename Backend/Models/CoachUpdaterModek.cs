d800d7b0cc175f9abfabdd02a9d73fa40429d3a9
seifeldin1 <seif1442004@gmail.com>
2024-12-25 09:11:12 +0200

    Fixed Coach CRUD functionalities

--- Backend/Models/CoachUpdaterModek.cs
+++ Backend/Models/CoachUpdaterModek.cs
@@ -1,20 +1,20 @@
 
 namespace Backend.Models 
 {
-    public class CoachModel : UserModel
+    public class CoachUpdaterModel : UserUpdaterModel
     {
-        public int Coach_ID { get; set; }
-        public int Salary { get; set; }
-        public int Penalties { get; set; }
-        public int Bonuses { get; set; }
-        public DateOnly Hire_Date { get; set; }
+        public int? Coach_ID { get; set; }
+        public int? Salary { get; set; }
+        public int? Penalties { get; set; }
+        public int? Bonuses { get; set; }
+        public DateOnly? Hire_Date { get; set; }
         public DateOnly? Fire_Date { get; set; }
         public int? Experience_Years { get; set; }
         public int? Works_For_Branch { get; set; }
-        public int Daily_Hours_Worked { get; set; }
+        public int? Daily_Hours_Worked { get; set; }
         public TimeSpan? Shift_Start { get; set; }
         public TimeSpan? Shift_Ends { get; set; }
-        public string Speciality { get; set; }
+        public string? Speciality { get; set; }
         public string? Status { get; set; }       //Can Have three values (OFF , Working , In a Meeting)
         public int? Contract_Length { get; set; } //can be null as coach may have left the gym as we keep track of fire date
     }
--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91332ac55f176049a6f8b52e0eec4ac3d3d6d698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c52230dbdab7a2a5f3c1d3414705bab166a36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
