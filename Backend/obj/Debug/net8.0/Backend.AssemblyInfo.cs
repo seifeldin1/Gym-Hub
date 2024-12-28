52cbd6ee508d7e6dd089daf83bd641b724337faf
seifeldin1 <seif1442004@gmail.com>
2024-12-28 08:39:06 +0200

    Front

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44f69c656a68dc92bd40d547c744b12aafb835d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7538acd82204a88546aa2a9ff973a9c91795a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
