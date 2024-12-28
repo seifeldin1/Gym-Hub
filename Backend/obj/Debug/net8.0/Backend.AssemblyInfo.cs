3aa3037847794fafd99fd31b92d446e233e08386
EngSeif <eng.seifmohamed2004@gmail.com>
2024-12-28 08:03:15 +0200

    add paths

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a0f65890c06d487476f63ee81a42f97ae32ba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d762aa18d7da5a1e5580f3a22b8c9f488c14d168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
