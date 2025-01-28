732f078a332a5473edc9270723eecaacb38d4475
EngSeif <eng.seifmohamed2004@gmail.com>
2025-01-28 16:06:56 +0300

    Init New Nextjs Folder

---
+++ frontend_enhanced/src/app/layout.jsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css';
+
+export const metadata = {
+  title: 'Pulse Fit',
+  description: 'Gym Management System',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Include metadata */}
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
