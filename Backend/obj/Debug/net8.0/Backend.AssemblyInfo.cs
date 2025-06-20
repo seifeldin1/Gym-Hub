35c58fffb2f8dc521a03ca7f144ce31565974090
seifeldin1 <seif1442004@gmail.com>
2025-06-20 18:21:26 +0300

    initial commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ae7d28608e3237318a6f19e606dbec8c2f980a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68ebe320e4322480853744ce3383b57db3d4543")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
