4fe017bd6457017fed3056fe2ad483bb5b0ef2fa
‪Amr Ashraf‬‏ <152652491+amrashraf15@users.noreply.github.com>
2025-06-22 20:42:25 +0300

    add EquipmentUpdaterModel

---
+++ Backend/Models/EquipmentsUpdaterModel.cs
@@ -0,0 +1,16 @@
+namespace Backend.Models
+{
+    public class EquipmentsUpdaterModel
+    {
+        public int?  Equipment_ID {get; set;}
+        public  string? Status { get; set; }
+        public int? Purchase_Price { get; set; }
+        public string? Category { get; set; }
+        public DateTime? Purchase_Date { get; set; }
+        public  string? Name { get; set; }
+        public  string? Serial_Number { get; set; }
+        public int? Belong_To_Branch_ID { get; set; }
+        
+    
+    }
+}
